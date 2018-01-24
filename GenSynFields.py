0e51aae373650fc402287ef500f245a8d19b451a
meade <brendanjmeade@gmail.com>
2018-01-24 16:58:49 -0500

    Loops and sums over 500 point sources

--- GenSynFields.py
+++ GenSynFields.py
@@ -24,7 +24,7 @@ tick_width = 0.5
 # Constants
 MU0 = 4 * np.pi * 1e-7
 LIFTOFF = 1e-6
-LEVELS = np.linspace(-1e-8, 1e-8, 20)
+LEVELS = np.linspace(-1e-6, 1e-6, 30)
 
 
 def calc_dipole_parameters(declination, inclination, moment_scalar):
@@ -32,9 +32,9 @@ def calc_dipole_parameters(declination, inclination, moment_scalar):
     theta = np.deg2rad(-inclination + 90)
     phi = np.deg2rad(-declination + 90)
 
-    moment_scalar = 1e-13
-    theta = np.deg2rad(-45)
-    phi = np.deg2rad(-45)
+    # moment_scalar = 1e-13
+    # theta = np.deg2rad(-45)
+    # phi = np.deg2rad(-45)
 
     moment_vector = np.zeros(3)
     moment_vector[0] = moment_scalar * np.sin(theta) * np.cos(phi)
@@ -55,14 +55,18 @@ def calc_observation_grid(x_bound, y_bound):
 def calc_point_source_field(moment_vector, x_source, y_source, z_raw, x_grid, y_grid):
     '''Compute the field of a magnetic dipole point source'''
     z_observed = z_raw * LIFTOFF
-    squared_distance = ((x_grid-x_source)**2.0 + (y_grid-y_source)**2.0 + z_observed**2.0)
+    squared_distance = (x_grid-x_source)**2.0 + \
+                       (y_grid-y_source)**2.0 + \
+                       z_observed**2.0
     
     aux = (moment_vector[0] * (x_grid - x_source) +
            moment_vector[1] * (y_grid - y_source) +
            moment_vector[2] * z_observed) / \
            squared_distance**(5.0 / 2.0)
+
     bz_dip = MU0 / (4.0 * np.pi) * \
-            (3.0 * aux * z_observed - moment_vector[2] / squared_distance**(3.0 / 2.0))
+             (3.0 * aux * z_observed - moment_vector[2] / squared_distance**(3.0 / 2.0))
+
     return bz_dip
 
 
@@ -133,7 +137,7 @@ def field_from_point_source_dict(point_source, x_grid, y_grid, z_raw):
 
 def main():
     '''Generate random fields'''
-    n_points = 1
+    n_points = 500
     x_bound = 500.0e-6
     y_bound = x_bound
     z_raw = 110 # microns
